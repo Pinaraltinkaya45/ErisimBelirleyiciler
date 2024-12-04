371d816b20409e9b135deafd3ea2949e4ca06ae0
Pinaraltinkaya45 <189837954+Pinaraltinkaya45@users.noreply.github.com>
2024-12-04 18:21:57 +0300

    Add project files.

---
+++ ErisimBelirleyiciler/Isci.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ErisimBelirleyiciler
+{
+    public class Isci
+    {
+        public int id = 1;
+        public string isim = "Sercan";
+        public string soyisim { get;set; }
+
+        public void bilgileriGoster()
+        {
+            Console.WriteLine("İşci: " + id);
+            Console.WriteLine("İsim: " + isim);
+            Console.WriteLine("Soyisim: " + soyisim);
+
+        }
+
+        public void deneme()
+        {
+            Console.WriteLine("deneme");
+        }
+    }
+}
---
+++ ErisimBelirleyiciler/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ErisimBelirleyiciler
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Isci isci1 = new Isci();
+            isci1.bilgileriGoster();
+            Console.WriteLine(isci1.id);
+            Console.WriteLine(isci1.isim);
+            isci1.soyisim = "Bayram";  // değer atadik
+            Console.WriteLine(isci1.soyisim); // atanan değeri aldık
+
+
+            Console.ReadLine();
+        }
+    }
+}
